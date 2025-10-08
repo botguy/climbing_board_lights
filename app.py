727e905d70288b69cf59ad3be1de8ea0981bebc2
Michael Nicholls <botguy@gmail.com>
2025-10-08 12:07:56 -0600

    diag_rainbow

--- app.py
+++ app.py
@@ -3,9 +3,11 @@
 from flask import Flask, render_template, request, jsonify
 import json
 import os
+from colorsys import hsv_to_rgb
 
 import neopixel
 import board
+from adafruit_led_animation.grid import PixelGrid
 
 # Configuration constants
 NUM_LEDS = 100
@@ -13,8 +15,13 @@ LED_PIN = board.D18
 ROWS, COLS = 12, 7
 STATE_NAMES = ["off", "hand", "foot", "start", "end"]
 
-assert ROWS*COLS <= NUM_LEDS
-led = neopixel.NeoPixel(LED_PIN, NUM_LEDS, pixel_order=neopixel.RGB)
+# setup LEDs
+LED_ROWS = ROWS
+LED_COLS = COLS + 1  # +1 since LEDS are on both sides of the holds
+assert LED_ROWS*LED_COLS <= NUM_LEDS
+led_strip = neopixel.NeoPixel(LED_PIN, NUM_LEDS, pixel_order=neopixel.RGB, auto_write=False)
+led_grid = PixelGrid(led_strip, LED_ROWS, LED_COLS)
+
 app = Flask(__name__)
 
 # Initialize grid
@@ -57,6 +64,15 @@ def load_pattern():
         return jsonify({"grid": grid})
     return jsonify({"error": "not found"}), 404
 
+def diag_rainbow():
+    ''' configures LEDs with a diagonal rainbow '''
+    for r in range(LED_ROWS):
+        for c in range(LED_COLS):
+            hue = (r*LED_ROWS + c*LED_COLS) / (LED_ROWS**2 + LED_COLS**2)  # inner product with diag vector
+            rgb = hsv_to_rgb(hue, 1, 1)
+            led_grid[r][c] = [int(255*rgb_i) for rgb_i in rgb]
+    led_strip.show()
+
 if __name__ == "__main__":
-    led[0] = (255, 255, 255)
+    diag_rainbow()
     app.run(host="0.0.0.0", port=80, debug=True)
